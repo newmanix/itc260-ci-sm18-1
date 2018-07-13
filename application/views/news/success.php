cb8e6f312a28b976232eba2872ce8d0d773d6b43
Bill Newman <william.newman@seattlecolleges.edu>
2018-07-12 19:26:49 -0700

    Initial commit for the News controller/model

---
+++ application/views/news/success.php
@@ -0,0 +1,8 @@
+<?php
+//application/views/news/success.php
+?>
+
+<p>Yay!  You entered data!</p>
+<p>Wouldn't it be great, if we showed it to you?</p>
+<p><a href="<?php echo site_url('news/')?>">View News</a></p>
+<p><a href="<?php echo site_url('news/create/')?>">Create another?</a></p>
